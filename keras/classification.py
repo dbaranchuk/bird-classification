4bca5cecfffd904968ff91b458ca5d6c8fd3dab3
Dmitry Baranchuk <dmitrybaranchuk@gmail.com>
2016-12-11 03:56:58 +0400

    Add files via upload

--- keras/classification.py
+++ keras/classification.py
@@ -1,68 +1,95 @@
 import numpy as np
-from skimage.transform import resize
+from skimage.transform import resize, rotate
 
 from keras.models import Sequential, Model
-from keras.layers import Dense
+from keras.layers import Dense, Activation
 from keras.optimizers import SGD
+
 from keras.utils.np_utils import to_categorical
 
 import matplotlib.pyplot as plt
-import h5py
+from keras.applications.vgg16 import VGG16
 
 from keras_vgg import vgg16
 from keras.applications.vgg16 import preprocess_input
+from keras.callbacks import Callback
+
+from numpy.random import uniform as rnd
 
 IMAGE_SIZE = 224
-VAL_SIZE = 500
 
 NUM_CLASSES = 50
-PER_CLASS = 30
-TRAIN_SIZE_PER_CLASS = 20
+PER_CLASS = 40
+TRAIN_SIZE_PER_CLASS = 40 #30
 
 BATCH_SIZE = 32
-N_EPOCH = 10
+N_EPOCH = 20
 
 model_dir = '/Users/dmitrybaranchuk/cv/cvintro2016/hw-07/vgg16_weights.h5'
 
+P = 0.5
+
+class SGDLearningRateTracker(Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        optimizer = self.model.optimizer
+        lr = (optimizer.lr * (1. / (1. + optimizer.decay * optimizer.iterations))).eval()
+        print('\nLR: {:.8f}'.format(float(lr)))
+
 class BirdClassifier:
-    
     def __init__(self):
         self.model = vgg16(model_dir)
         self.model.pop()
         self.model.pop()
         self.model.add(Dense(NUM_CLASSES, name='new_dense'))
         self.model.add(Activation('softmax', name='softmax'))
-        
-        for i, layer in enumerate(self.model.layers):
-            print(i, layer.name)
 
     def set_data(self, X, y=None):
         if y is None:
             self.X_test = X
         else:
-            self.X_train, self.y_train = ([], [])
-            self.X_val, self.y_val = ([], [])
-            X, y = list(X), list(y)
+            self.X_train, self.y_train = (X, y) #([], [])
+        #self.X_val, self.y_val = ([], [])
+        #            X, y = list(X), list(y)
+        
+        #for i in range(NUM_CLASSES):
+        #       self.X_train += X[i*PER_CLASS: i*PER_CLASS + TRAIN_SIZE_PER_CLASS]
+        #       self.y_train += y[i*PER_CLASS: i*PER_CLASS + TRAIN_SIZE_PER_CLASS]
+    
+        #       self.X_val += X[i*PER_CLASS + TRAIN_SIZE_PER_CLASS: (i+1)*PER_CLASS]
+        #           self.y_val += y[i*PER_CLASS + TRAIN_SIZE_PER_CLASS: (i+1)*PER_CLASS]
+    
+        #self.X_train, self.y_train = np.array(self.X_train), np.array(self.y_train)
+            #self.X_val, self.y_val = np.array(self.X_val), np.array(self.y_val)
             
-            for i in range(NUM_CLASSES):
-                self.X_train += X[i*PER_CLASS: i*PER_CLASS + TRAIN_SIZE_PER_CLASS]
-                self.y_train += y[i*PER_CLASS: i*PER_CLASS + TRAIN_SIZE_PER_CLASS]
-    
-                self.X_val += X[i*PER_CLASS + TRAIN_SIZE_PER_CLASS: (i+1)*PER_CLASS]
-                self.y_val += y[i*PER_CLASS + TRAIN_SIZE_PER_CLASS: (i+1)*PER_CLASS]
-    
-            self.X_train, self.y_train = np.array(self.X_train), np.array(self.y_train)
-            self.X_val, self.y_val = np.array(self.X_val), np.array(self.y_val)
-    
+            print(self.X_train.shape, self.y_train.shape)
+
+    def augment_data(self):
+        X_aug, y_aug = ([],[])
+        
+        for i in range(len(self.X_train)):
+            new_X = [self.X_train[i]]
+            new_y = [self.y_train[i]]
+            
+            if rnd(0, 1) < P:
+                new_X.append(new_X[0][:,::-1,:])
+                new_y.append(new_y[0])
+                    
+            X_aug += new_X
+            y_aug += new_y
+                
+        self.X_train = np.array(X_aug)
+        self.y_train = np.array(y_aug)
+
     def train(self, mode):
         if mode == 'FC':
-            for layer in self.model.layers[:32]:
+            for layer in self.model.layers[:24]:
                 layer.trainable = False
             
-            sgd = SGD(lr=0.0001, decay=1e-6, momentum=0.9)
+            sgd = SGD(lr=0.0001, decay=1e-3, momentum=0.9)
             self.model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
             self.model.fit(self.X_train, self.y_train, batch_size=BATCH_SIZE,
-                       nb_epoch=N_EPOCH, validation_data=(self.X_val, self.y_val))
+                           callbacks=[SGDLearningRateTracker()],
+                           nb_epoch=N_EPOCH)#, validation_data=(self.X_val, self.y_val))
 
     def predict(self):
         return self.model.predict_classes(self.X_test, batch_size=BATCH_SIZE)
@@ -77,26 +104,26 @@ def resize_images(X, b):
     
     return np.array(X).transpose(0, 3, 1, 2)
 
-
 def train_classifier(X, b, y):
     model = BirdClassifier()
     print("Initialization is completed")
 
     X = resize_images(X, b)
-    X = preprocess_input(X)
-    y = to_categorical(y-1, 50)
+    X = preprocess_input(255.*X)
+    y = to_categorical(y-1, NUM_CLASSES)
     print("Preprocessing is completed")
     
-    print(X.shape, y.shape)
-    
     model.set_data(X, y)
+    model.augment_data()
+    print("Augmentation is completed")
+    
     model.train('FC')
+    
     return model
-
 
 def predict(model, X, b):
     X = resize_images(X, b)
-    X = preprocess_input(X)
+    X = preprocess_input(255.*X)
     
     model.set_data(X)
     return model.predict()+1
